9110ad64615ea844339b16c06b485a9ae14bdbe0
vesu-dev <174601598+vesu-dev@users.noreply.github.com>
2025-03-03 15:52:57 +0000

    Fix ci

--- scripts/checkShutdownStatus.ts
+++ scripts/checkShutdownStatus.ts
@@ -1,4 +1,4 @@
-import { CallData, hash, shortString } from "starknet";
+import { shortString } from "starknet";
 import { setup, toI257 } from "../lib";
 
 const deployer = await setup(process.env.NETWORK);
@@ -23,14 +23,14 @@ console.log("Checking shutdown status for each pair...");
 for (const [, asset] of pool.params.ltv_params.entries()) {
   let collateral_asset = assets[asset.collateral_asset_index];
   let debt_asset = assets[asset.debt_asset_index];
-  const collateral_asset_symbol = shortString.decodeShortString((await collateral_asset.symbol()));
-  const debt_asset_symbol = shortString.decodeShortString((await debt_asset.symbol()));
+  const collateral_asset_symbol = shortString.decodeShortString(await collateral_asset.symbol());
+  const debt_asset_symbol = shortString.decodeShortString(await debt_asset.symbol());
 
   console.log("");
   console.log(`  • ${collateral_asset_symbol} / ${debt_asset_symbol}`);
 
   const context = await singleton.context_unsafe(pool.id, collateral_asset.address, debt_asset.address, "0x0");
-  
+
   const collateral_asset_price = Number(context.collateral_asset_price.value) / 1e18;
   const debt_asset_price = Number(context.debt_asset_price.value) / 1e18;
 
@@ -58,30 +58,50 @@ for (const [, asset] of pool.params.ltv_params.entries()) {
     console.log("    Invalid debt rate_accumulator");
     console.log("      rate_accumulator: ", debt_accumulator);
   }
-  
+
   const pair = await extensionPO.pairs(pool.id, collateral_asset.address, debt_asset.address);
-  const collateral = Number(await singleton.calculate_collateral_unsafe(pool.id, collateral_asset.address, toI257(pair.total_collateral_shares))) / Number(context.collateral_asset_config.scale);
-  const debt = Number(await singleton.calculate_debt(toI257(pair.total_nominal_debt), context.debt_asset_config.last_rate_accumulator, context.debt_asset_config.scale)) / Number(context.debt_asset_config.scale);
+  const collateral =
+    Number(
+      await singleton.calculate_collateral_unsafe(
+        pool.id,
+        collateral_asset.address,
+        toI257(pair.total_collateral_shares),
+      ),
+    ) / Number(context.collateral_asset_config.scale);
+  const debt =
+    Number(
+      await singleton.calculate_debt(
+        toI257(pair.total_nominal_debt),
+        context.debt_asset_config.last_rate_accumulator,
+        context.debt_asset_config.scale,
+      ),
+    ) / Number(context.debt_asset_config.scale);
   const collateral_value = collateral * collateral_asset_price;
   const debt_value = debt * debt_asset_price;
-  
-  const shutdown_ltv = (debt_value === 0) ? 0 : debt_value / collateral_value;
-  const shutdown_ltv_max = Number((await extensionPO.shutdown_ltv_config(pool.id, collateral_asset.address, debt_asset.address)).max_ltv) / 1e18;
+
+  const shutdown_ltv = debt_value === 0 ? 0 : debt_value / collateral_value;
+  const shutdown_ltv_max =
+    Number((await extensionPO.shutdown_ltv_config(pool.id, collateral_asset.address, debt_asset.address)).max_ltv) /
+    1e18;
 
   if (shutdown_ltv >= shutdown_ltv_max) {
     console.log("    Shutdown LTV is greater than or equal to the max LTV");
     console.log("      shutdown_ltv:    ", shutdown_ltv);
     console.log("      shutdown_ltv_max:", shutdown_ltv_max);
   }
-  
+
   const shutdown_status = await extensionPO.shutdown_status(pool.id, collateral_asset.address, debt_asset.address);
 
   if (shutdown_status.violating) {
     console.log("    Shutdown status is violating");
     console.log("      shutdown_mode:", shutdown_status.shutdown_mode);
   }
-  
-  const violation_timestamp = await extensionPO.violation_timestamp_for_pair(pool.id, collateral_asset.address, debt_asset.address);
+
+  const violation_timestamp = await extensionPO.violation_timestamp_for_pair(
+    pool.id,
+    collateral_asset.address,
+    debt_asset.address,
+  );
 
   if (violation_timestamp !== 0n) {
     console.log("    Violation timestamp found");
